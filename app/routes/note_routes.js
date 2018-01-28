97c62e71fa929d9636e162357dd3fea7592c68ce
KokuKUSIAKU <kusiaku.k@gmail.com>
2018-01-28 17:03:03 +0000

    comment cleanup

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,25 +1,13 @@
 module.exports = function(app, db) {
-  //console.log("db collection", db.collection);
   app.post('/notes', (req, res) => {
-    console.log("reques", req);
     const note = { text: req.body.body, title: req.body.title };
-    console.log(note);
-    //console.log("db collection", db.collection);
     db.collection("todos").insertOne(note,(err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
-        //res.send(result.ops[0]);
+        //you can send res.body to the requester
       }
     });
-    /*db.collection('todos').insertOne(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-        res.send(result.ops[0]);
-      }
-    });*/
-    //console.log("db in post request ", db.collection);
     res.send("hello");
   });
 };
